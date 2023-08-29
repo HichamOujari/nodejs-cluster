49990e104207e07072721b951d2e8e1febfd86b9
hichamoujari <hicham.oujari@izi.net>
2023-08-29 18:23:27 +0100

    random mesg

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const cluster = require("cluster");
 const os = require("os");
 const countCpus = os.cpus().length;
 
-if(cluster.isMaster) for (let i = 0; i < countCpus; i++)  cluster.fork();
+if(cluster.isMaster) {
+  for (let i = 0; i < countCpus; i++)  cluster.fork();
+  cluster.on("exit", (worker) => cluster.foker());
+}
+else {
+  const port = process.env.port || 3000;
 
-const port = process.env.port || process.pid;
-
-app.get("/non-blocking", (req, res) => {
-  console.log("====> new Request in : ", process.pid);
-  res.send("Non blocking api");
-});
-
-app.get("/blocking", (req, res) => {
-  let counter = 0;
-  for (let i = 0; i < 2000000000; i++) {
-    counter++;
-  }
-  res.status(200).send(`Blocking api (message = ${counter})`);
-});
-
-app.listen(port, () => {
-  console.log(
-    `The application is starting with (port = ${port}) (pid = ${process.pid})`
-  );
-});
-
-cluster.on("exit", (worker) => cluster.foker());
+  app.get("/non-blocking", (req, res) => {
+    res.send("Non blocking api");
+  });
+  
+  app.get("/blocking", (req, res) => {
+    let counter = 0;
+    for (let i = 0; i < 200000000; i++) {
+      counter++;
+    }
+    res.status(200).send(`Blocking api (message = ${counter})`);
+  });
+  
+  app.listen(port, () => {
+    console.log(
+      `The application is starting with (port = ${port}) (pid = ${process.pid})`
+    );
+  });
+}
